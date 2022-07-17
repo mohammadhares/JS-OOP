89dd18b70e57bb6385d768c073eed2af12468e28
Muhammad Haris <muhammadhares11@gmail.com>
2022-07-17 23:40:32 +0430

    First Release

---
+++ App/app.js
@@ -0,0 +1,47 @@
+// Object Literal
+// const Student = {
+//     firstname: 'ahmad',
+//     watch: function(){
+//         console.log('Watch');
+//     }
+// }
+
+// Factory Function
+// function Student(firstname , lastname){
+//     return{
+//         firstname: firstname,
+//         lastname: lastname,
+//         showInfo(){
+//             console.log(firstname, lastname)
+//         }
+//     }
+// }
+
+
+// let std = Student.call({}, 'ahamad', 'karim');
+// console.log(std.firstname);
+// let std = Student('Haris', 'Muhammadi')
+// console.log(std.firstname);
+// console.log(std.lastname);
+// std.showInfo();
+
+
+// Constructor Function
+// function Student(firstname, lastname){
+//     this.firstname = firstname;
+//     this.lastname = lastname;
+//     this.showInfo = function(){
+//         console.log('Show Info...', this.firstname);
+//     }
+// }
+
+
+// let std = new Student("haris", "muhammadi");
+// console.log(std.firstname);
+
+
+// let std = new Student('haris', 'muhammadi');
+// console.log(std.firstname);
+// console.log(std.lastname);
+// std.showInfo();
+
